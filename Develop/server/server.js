c7e9e1585a5c1bef4142bd0d70ea0c4c6a8d621e
Ryan Hood <114943155+ryanhood10@users.noreply.github.com>
2023-03-23 15:11:01 -0500

    set up server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,52 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer, gql } = require('apollo-server-express');
 const db = require('./config/connection');
 const routes = require('./routes');
+
+// Define your GraphQL type definitions and resolvers here
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello, world!",
+  },
+};
+
+// Create an instance of ApolloServer
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Function to start the server
+const startServer = async () => {
+  // Wait for the ApolloServer to start
+  await server.start();
+  
+  // Apply the ApolloServer middleware to the Express app
+  server.applyMiddleware({ app });
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-app.use(routes);
+  // If we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.use(routes);
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`🚀 GraphQL Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+};
+
+// Call the startServer function to start the server
+startServer();
+                                                            
